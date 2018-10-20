1714f1e090b78b3dd630247654fffbe9c06770d9
abghorba <andrewbghorbani@gmail.com>
2018-10-19 21:36:22 -0700

    Implemented the main class for the player vs computer TTT game

--- UnbeatableTTT.java
+++ UnbeatableTTT.java
@@ -18,9 +18,54 @@ public class UnbeatableTTT
 
         board.printGameBoard();
 
-        while(keepPlaying)
-        {
+        while(keepPlaying) {
+            if(board.isGameOver()) {
+                if (board.isAWin())
+                    System.out.println("The winner is: " + winner);
+                else if (board.isADraw())
+                    System.out.println("Its a DRAW!");
 
+                System.out.print("Do you want to play again? (y/n)");
+                String playAgain = in.next();
+                if (playAgain.equals("y")) {
+                    board.resetGameBoard();
+                    board.printGameBoard();
+                    continue;
+                }
+                else {
+                    keepPlaying = false;
+                    System.out.println("Thanks for playing!");
+                    continue;
+                }
+            }
+
+            // Player 1's turn
+            System.out.println("Player 1 enter the row and column: ");
+            do {
+                row = in.nextInt()-1;
+                col = in.nextInt()-1;
+            } while(!player1.isValidMove(row, col));
+            player1.makeMove(row, col);
+            board.moves++;
+            board.printGameBoard();
+            if(board.isAWin()) {
+                winner = "Player 1!";
+                continue;
+            }
+            else if(board.isADraw())
+                continue;
+
+
+            // Computer's turn
+            computer.makeMove();
+            board.moves++;
+            board.printGameBoard();
+            if(board.isAWin()) {
+                winner = "Computer!";
+                continue;
+            }
+            else if(board.isADraw())
+                continue;
         }
     }
 }
